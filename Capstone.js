f0787ce83ffb72190221839f3323775a2c6f7f66
Ayanacyan <amitpacl@gmail.com>
2025-11-16 16:35:05 -0600

    Explore page

--- Capstone.js
+++ Capstone.js
@@ -1,3 +1,23 @@
 function myLibrary(){
   
 }
+ 
+
+var myForm= document.getElementById("myForm");
+            var input= document.getElementById("thing");
+            var Item= document.getElementById("list");
+            myForm.addEventListener("submit", 
+            function(event){
+                event.preventDefault()
+                createItem(thing.value)
+            })
+            function createItem(inputItems){
+                var items=`<li>${inputItems}
+                    <button onclick="deleteElement(this)">delete</button> </li>`
+                    Item.insertAdjacentHTML("beforeend", items)
+                    input.value=""
+                    input.focus()
+            }
+            function deleteElement(ElementToDelete){
+                ElementToDelete.parentElement.remove()
+            }
